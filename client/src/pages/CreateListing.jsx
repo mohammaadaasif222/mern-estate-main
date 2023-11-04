15a4c43f158052232fd208e7aa3bb2da03e87276
Mohammad Aasif <kasifsaif784@gmail.com>
2023-11-04 10:08:37 +0530

    frontend changed

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -137,7 +137,7 @@ export default function CreateListing() {
         return setError('Discount price must be lower than regular price');
       setLoading(true);
       setError(false);
-      const res = await fetch('/api/listing/create', {
+      const res = await fetch('https://mohammad-estate.onrender.com/api/listing/create', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',

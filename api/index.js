39d22237913ae0b98f00959f5f6cd5b03d54f679
Mohammad Aasif <kasifsaif784@gmail.com>
2023-11-04 09:51:45 +0530

    chnaged

--- api/index.js
+++ api/index.js
@@ -39,6 +39,9 @@ app.use('/api/listing', listingRouter);
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 // })
+app.get('/', (req, res) => {
+  res.json("Hello, world!");
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
